7ec594b7b4b5c36d40bdcfbfd71a05fc73372a2f
Kaaleyah <41757315+Kaaleyah@users.noreply.github.com>
2022-08-15 11:37:31 +0300

    Added comments for script files

--- userA/scriptA.js
+++ userA/scriptA.js
@@ -1,32 +1,43 @@
+// Create a RTCPeerConnection object
 const localConnection = new RTCPeerConnection();
 
+// Create a data channel assigned to the localConnection object
 const dataChannel = localConnection.createDataChannel('dataChannel');
 
-// Event listeners
+//// Event listeners
+// When data channel is open, log out a message indicating that the data channel is open
 dataChannel.onopen = () => {
     console.log('dataChannel opened');
 }
 
+// When a message is received, log out the message
 dataChannel.onmessage = event => {
     console.log("New Message: " + event.data);
 }
 
 // Create offer
 localConnection.createOffer().then( offer => {
+    // Set the offer as local description
     localConnection.setLocalDescription(offer);
 }).then( a => {
+    // Log out a message indicating that the offer is created
     console.log("Offer created!");
 })
 
+// Listen for ICE candidates and add them to the localConnection object
 localConnection.onicecandidate = event => {
     console.log("New candidate! SDP updated: ");
     console.log(localConnection.localDescription);
 }
 
+// Get the answer offer and set it as remote description
 function setAnswer(answer) {
     localConnection.setRemoteDescription(answer);
 }
 
+// Send a message via the data channel
 function sendMessage(message) {
     dataChannel.send(message);
 }
+
+// For more detail, see README.md
--- userB/scriptB.js
+++ userB/scriptB.js
@@ -1,28 +1,36 @@
+// Create a RTCPeerConnection object
 const remoteConnection = new RTCPeerConnection();
 
+// Listen for new ICE candidates and add them to the remoteConnection object
 remoteConnection.onicecandidate = event => {
     console.log("New candidate! SDP updated: ");
     console.log(remoteConnection.localDescription);
 }
 
+// Listen for data channel changes 
 remoteConnection.ondatachannel = event => {
     const dataChannel = event.channel;
     remoteConnection.dataChannel = dataChannel;
+    // Assign the data channel to remoteConnection object
 
+    // Listen for messages and log them out
     dataChannel.onmessage = event => {
         console.log("New Message: " + event.data);
     }
 
+    // When data channel is open, log out a message indicating that the data channel is open
     dataChannel.onopen = () => {
         console.log('Data Channel opened');
     }
 }
 
+// Get the offer from other userA and set it as remote description
 function setOffer(offer) {
     remoteConnection.setRemoteDescription(offer).then( a => {
         console.log("Offer set!");
     })
 
+    // In response, create an answer and set it as local description
     remoteConnection.createAnswer().then( answer => {
         remoteConnection.setLocalDescription(answer);
     }).then( a => {
@@ -30,6 +38,9 @@ function setOffer(offer) {
     })
 }
 
+// Send a message via the data channel
 function sendMessage(message) {
     remoteConnection.dataChannel.send(message);
 }
+
+// For more detail, see README.md
